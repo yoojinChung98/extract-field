0a7beecccddd46aa46acfacaaad67eda2e4cdd7e
CT00375 <yjjeong@nextree.io>
2025-05-02 13:33:31 +0900

    Acton 클래스 추가

---
+++ src/main/java/com/project/extractfield/ExtractFieldAction.java
@@ -0,0 +1,13 @@
+package com.project.extractfield;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+public class ExtractFieldAction extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        //
+        System.out.println("Action 수행 완료!");
+    }
+}
